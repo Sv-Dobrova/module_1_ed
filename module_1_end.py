55185ba59b32c9c88225730dff3b2364995aa563
Sv-Dobrova <sv-dobrova@yandex.ru>
2024-10-01 21:14:32 +0700

    Add files via upload

---
+++ module_1_end.py
@@ -0,0 +1,9 @@
+students = ['Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron']
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+#while =
+grades_middle = sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])
+#print(grades_middle)
+students.sort()
+#print(students)
+result_dict = dict(zip(students, grades_middle))
+print(result_dict)
